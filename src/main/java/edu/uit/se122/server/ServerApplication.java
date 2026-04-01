efbbed2cb0e0695f8729b79ac615897c93680d0c
Brakenull <brakenull@gmail.com>
2026-04-02 02:11:19 +0700

    Create folder structure

--- src/main/java/edu/uit/se122/server/ServerApplication.java
+++ src/main/java/edu/uit/se122/server/ServerApplication.java
@@ -3,10 +3,13 @@ package edu.uit.se122.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ServerApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
         SpringApplication.run(ServerApplication.class, args);
     }
 
---
+++ src/main/java/edu/uit/se122/server/promotion/PromotionController.java
@@ -0,0 +1,4 @@
+package edu.uit.se122.server.promotion;
+
+public class PromotionController {
+}
